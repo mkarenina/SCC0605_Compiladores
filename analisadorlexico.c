2f43d8d389c34bbc0b565ed6ea6df2e3ea76e58e
Marianna Karenina <40308025+mkarenina@users.noreply.github.com>
2022-05-29 19:34:59 -0300

    Update analisadorlexico.c

--- analisadorlexico.c
+++ analisadorlexico.c
@@ -11,8 +11,9 @@
 #include <stdlib.h>
 #include <string.h>
 #define Tamanho_Maximo 50
+#define Tamanho_Hash 17
 
-char *lido
+char *lido;
 
 void escreve_ArqSaida(char *palavra, FILE *saida){
 
@@ -24,99 +25,130 @@ void escreve_ArqSaida(char *palavra, FILE *saida){
     return;
 }
 
+void escreve_ArqSaida_TamMax(char *lido, FILE *saida){
+
+    fputs(lido, saida);
+	fputs(", Tamanho Maximo Atingido\n", saida);
+	
+    return;
+}
+
+
 //------------------------- Confere Palavras Reservadas -------------------------------- 
 void palavras_reservadas(char *palavra, FILE *saida){
 
-    switch(palavra){
 
-        case 'program':
-            escreve_ArqSaida(palavra, saida);
-            break;
+	if((strcmp(palavra, "program")) == 0){
 
-        case 'var':
-            escreve_ArqSaida(palavra, saida);
-            break;
-        
-        case 'begin':
-            escreve_ArqSaida(palavra, saida);
-            break;
-        
-        case 'to':
-            escreve_ArqSaida(palavra, saida);
-            break;
-        
-        case 'do':
-            escreve_ArqSaida(palavra, saida);
-            break;
+		escreve_ArqSaida(palavra, saida);
+		return;
+	}
 
-        case 'else':
-            escreve_ArqSaida(palavra, saida);
-            break;
-        
-        case 'then':
-            escreve_ArqSaida(palavra, saida);
-            break;
-        
-        case 'if':
-            escreve_ArqSaida(palavra, saida);
-            break;
-        
-        case 'procedure':
-            escreve_ArqSaida(palavra, saida);
-            break;
+	if((strcmp(palavra, "var")) == 0){
+		escreve_ArqSaida(palavra, saida);
+		return;
+	}
 
-        case 'integer':
-            escreve_ArqSaida(palavra, saida);
-            break;
-        
-        case 'real':
-            escreve_ArqSaida(palavra, saida);
-            break;
-        
-        case 'const':
-            escreve_ArqSaida(palavra, saida);
-            break;
-        
-        case 'while':
-            escreve_ArqSaida(palavra, saida);
-            break;
+	if((strcmp(palavra, "begin")) == 0){
+		escreve_ArqSaida(palavra, saida);
+		return;
+	}
 
-        case 'for':
-            escreve_ArqSaida(palavra, saida);
-            break;
-        
-        case 'read':
-            escreve_ArqSaida(palavra, saida);
-            break;
-        
-        case 'write':
-            escreve_ArqSaida(palavra, saida);
-            break;
-        
-        case 'end':
-            escreve_ArqSaida(palavra, saida);
-            break;  
+	if((strcmp(palavra, "to")) == 0){
+		escreve_ArqSaida(palavra, saida);
+		return;
+	}
 
-        default:
-            fputs(palavra, saida);
-	        fputs(", id\n", saida); 
-    }
+	if((strcmp(palavra, "do")) == 0){
+		escreve_ArqSaida(palavra, saida);
+		return;
+	}
 
-    return;
+	if((strcmp(palavra, "else")) == 0){
+		escreve_ArqSaida(palavra, saida);
+		return;
+	}
 
+	if((strcmp(palavra, "then")) == 0){
+		escreve_ArqSaida(palavra, saida);
+		return;
+	}
+
+	if((strcmp(palavra, "if")) == 0){
+		escreve_ArqSaida(palavra, saida);
+		return;
+	}
+
+	if((strcmp(palavra, "procedure")) == 0){
+		escreve_ArqSaida(palavra, saida);
+		return;
+	}
+
+	if((strcmp(palavra, "begin")) == 0){
+		escreve_ArqSaida(palavra, saida);
+		return;
+	}
+
+	if((strcmp(palavra, "integer")) == 0){
+		escreve_ArqSaida(palavra, saida);
+		return;
+	}
+
+	if((strcmp(palavra, "const")) == 0){
+		escreve_ArqSaida(palavra, saida);
+		return;
+	}
+
+	if((strcmp(palavra, "while")) == 0){
+		escreve_ArqSaida(palavra, saida);
+		return;
+	}
+
+	if((strcmp(palavra, "for")) == 0){
+		escreve_ArqSaida(palavra, saida);
+		return;
+	}
+
+	if((strcmp(palavra, "read")) == 0){
+		escreve_ArqSaida(palavra, saida);
+		return;
+	}
+
+	if((strcmp(palavra, "write")) == 0){
+		escreve_ArqSaida(palavra, saida);
+		return;
+	}
+
+	if((strcmp(palavra, "end")) == 0){
+		escreve_ArqSaida(palavra, saida);
+		return;
+	}
+	
+	fputs(palavra, saida);
+	fputs(", id\n", saida);
+	fputs(palavra, saida);
+
+	return;
 }
+
+
 
 int lexico(FILE *entrada, FILE *saida){
     int lendo = 0;
-    int i, j = 0;
-    int flag_pos, flag_tamMax = 0;
-    char aux;
+    int i = 0; 
+	int j = 0;
+    int flag_pos = 1;
+    int flag_tamMax = 0;
+	int int_ou_real = 0;
 
 //------------- Automato Identificador -----------------------
     if((lido[i]>64 && lido[i]<91) || (lido[i]>96 && lido[i]<123)){ //conferindo valor ASCII
-			//i++;
-			lido=fscanf(entrada, "%c", &lido[1]);
-			while(((lido[i]>64 && lido[i]<91) || (lido[i]>96 && lido[i]<123) || (lido[i]>47 && lido[i]<58)) && flag_tamMax==0){
+			i++;
+			lendo =fscanf(entrada, "%c", &lido[1]);
+
+			int flag_tamMaximo = 0;
+
+			while(((lido[i]>64 && lido[i]<91) || (lido[i]>96 && lido[i]<123) || (lido[i]>47 && lido[i]<58)) && flag_tamMax == 0){
 				
                 i++;
 
@@ -135,11 +167,12 @@ int lexico(FILE *entrada, FILE *saida){
 					palavra[j]=lido[j];
 				}
 
-				aux = lido[i];
+				char aux = lido[i];
 				memset(lido, 0, Tamanho_Maximo*sizeof(char));
 				i=0;
 				lido[i]=aux;
 				flag_pos=0;
+				//printf("palavra: %s", palavra)
 				palavras_reservadas(palavra, saida);
 				free(palavra);
 			}
@@ -153,6 +186,72 @@ int lexico(FILE *entrada, FILE *saida){
 	}
 
 //--------------------- Automato Numeral ---------------------------
+if(lido[i]>47 && lido[i]<58){
+
+			//printf("entrou numeral\n");
+			
+			flag_tamMax = 0;		
+
+			while((lido[i]>47 && lido[i]<58) && flag_tamMax==0){
+				i++;
+				if(i<Tamanho_Maximo){
+					lendo=fscanf(entrada, "%c", &lido[i]);
+				}
+				else{
+					flag_tamMax=1;
+				}
+			}
+			if(lido[i]==46){//caso .
+				int_ou_real=1;
+				i++;		//fla2
+				if(i<Tamanho_Maximo){
+					lendo=fscanf(entrada, "%c", &lido[i]);
+				}
+				else{
+					flag_tamMax=1;
+				}
+				while((lido[i]>47 && lido[i]<58) && flag_tamMax==0){
+					i++;
+					if(i<Tamanho_Maximo){
+						lendo=fscanf(entrada, "%c", &lido[i]);
+					}
+					else{
+						flag_tamMax=1;
+					}
+				}
+			}
+			if(flag_tamMax==0){
+				for(int j=0; j<i; j++){
+					fprintf(saida, "%c", lido[j]);
+				}
+				char aux = lido[i];
+				memset(lido, 0, Tamanho_Maximo*sizeof(char));
+				i=0;
+				lido[i]=aux;
+				if(lido[i]==9 || lido[i]==10 || lido[i]==32 || (lido[i]>39 && lido[i]<48) || (lido[i]>57 && lido[i]<63) || lido[i]== 123){
+					if(!int_ou_real){
+						fputs(", numero_inteiro\n", saida);
+					}
+					else{
+						fputs(", numero_real\n", saida);
+					}
+				}
+				else{
+					if((lido[i]>64 && lido[i]<91) || (lido[i]>96 && lido[i]<123)){
+						fputs(", !!ERRO!! Caracter INVALIDO\n", saida);
+					}
+					else{
+						fputs(", !!ERRO!! Formatado invalido\n", saida);
+					}
+					
+				}
+				flag_pos=0;
+			}
+			else{
+				escreve_ArqSaida_TamMax(lido, saida);
+				i=0;
+			}
+		}
 
 //--------------------- Automato Operador --------------------------
 		if((lido[i]>39 && lido[i]<48) || (lido[i]>57 && lido[i]<63)){
@@ -184,7 +283,7 @@ int lexico(FILE *entrada, FILE *saida){
 					break;
 				case 58:	//caso :
 					lendo=fscanf(entrada, "%c", &lido[i]);
-					if(s[t]==61){
+					if(lido[i]==61){
 						fprintf(saida, "':=', simb_Atribuicao\n");
 					}
 					else{
@@ -196,7 +295,7 @@ int lexico(FILE *entrada, FILE *saida){
 					fprintf(saida, "';', simb_PontoVirgula\n");
 					break;
 				case 60:	//caso <
-					lendo=fscanf(ent, "%c", &lido[i]);
+					lendo=fscanf(entrada, "%c", &lido[i]);
 					if(lido[i]==62){
 						fprintf(saida, "'<>', simb_Relacao\n");
 					}
@@ -235,14 +334,30 @@ int lexico(FILE *entrada, FILE *saida){
 				lendo=fscanf(entrada, "%c", &lido[i]); //comentarios sao ignorados 
 			}		
 	}
+//-------------- Tratamento de Caracter Invalido
+	if((lido[i]>32 && lido[i]<40) || lido[i]== 63 || lido[i]== 64 || (lido[i]>90 && lido[i]<97) || lido[i]>124){
+		
+		//printf("entrou trata caracter\n");
 
+		if(lido[i]==125){
+			fprintf(saida, "%c, !!ERRO!! '}' sozinho. Comentario nao foi inicializado\n", lido[i]);
+		}
+		else{
+			fprintf(saida, "%c, !!ERRO!! Caracter INValido \n", lido[i]);
+			}
+		flag_pos=1;
+	} 
 
+//------------ Fim
+	if(flag_pos){	//flag_pos=1 ler novo caracter, flag_pos=0 caracter na memoria
 
+		//printf("entrou fim\n");
 
-
+		lendo=fscanf(entrada, "%c", &lido[i]);
+	}
+	
+	return lendo;
 }
-
-
 
 
 //------------- main ------------------
